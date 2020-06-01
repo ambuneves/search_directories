9403db0d2a0a5dacee03b7f4ae4820d7ceafac4a
Marta Vidal-Garcia <marta.vidalga@gmail.com>
2020-06-01 15:57:56 -0600

    Modifed the function to make it more streamlined. Hope that helps

--- search_directories_function.R
+++ search_directories_function.R
@@ -1,51 +1,41 @@
-search.directories <- funtion(dir = NULL, file_type, folders = NULL) {
-  
+search.directories <- function(dir = NULL, file_type, folders = NULL) {
   # Set up directory
   if (is.null(dir) == TRUE){
     path <- getwd() # current directory
-    setwd(path)
   }
   else {
     path <- dir # we indicate which directory
-    setwd(path)
   }
-  
   # set up which file type you are looking for
-  file <- paste(".",file_type, sep = "")
-  
+  file <- paste0(".",file_type) # same as paste(".",file_type, sep = ""), but simpler
   # Set up subfolder
-  if (is.null(subfolders) == TRUE){   #option a (NULL), default: look at all the subfolder within that directory
-    subfolders <- list.dirs(path = ".")[-1]
+  if (is.null(folders) == TRUE){   #option a (NULL), default: look at all the subfolder within that directory
+    subfolders <- list.dirs(path = path)[-1] # if you set is at 'path' instead of current directory, it is simpler and you do not need to setwd(path)
+    # As well as that, you end up getting a vector with full subfolder paths instead of relative paths
   }
   else {  # option b: give a vector of subfolders' relative path
-    all_folders <- list.dirs(path = ".")[-1]
-    subfolders <- all_folders[grep(pattern = folders, x = all_folders, ignore.case = TRUE)] #If folders are specified, search through all subfolders to find matches... maybe there's a better way to do this?
+    subfolders <- folders # what you had was good, but this is simpler (and shorter code). You just need to make sure the vector has the full subfolder paths
   }
-  
   #Set up the paths that the loop will use to search for files  
-  subpath <- vector(mode="character", length=(length(subfolders))) 
+  # subpath <- vector(mode="character", length=length(subfolders)) We don't need this'
   volume_data <- matrix(data=NA, nrow=0, ncol=4) #Initialize the matrix 
   colnames(volume_data) <- c("specimen","filename", "folder", paste(file_type, "path", sep = "_"))
-  
-  #Loop through directories and create dataframe from filenames 
-  for (i in 1: length(subfolders)){
-    subpath[i] <- paste0(path, sub(".", "", subfolders[i])) # we are removing the first character in the subfolder path
-    all.files <- list.files(path = subpath[i])
-    search.ids <- grep(pattern = paste0("\\", file), x = all.files, ignore.case = T)
-    search.files <- all.files[search.ids]
-    full_path <- Sys.glob(file.path(subpath[i], search.files)) 
-    setwd(subpath[i])
-    folder <- rep(substr(subfolders[i], start = 3, stop=nchar(subfolders[i])), length(search.files))# just the folder name
-    gsub_id <- paste('.{', nchar(as.character(file)), '}$', sep = '')
-    specimen <- gsub(pattern = gsub_id, '', search.files) #Get the specimen name, which is the file name minus the file extension
-    setwd(path)
-    temp <- cbind.data.frame(specimen, search.files, folder, full_path)
-    volume_data <- rbind(volume_data, temp)
+  # It would be better to create a temp matrix with the 4 columns you want and then append it.
+  for (i in 1:length(subfolders)) {
+    filename <- list.files(subfolders[i], pattern = file)
+    if (length(filename) > 0){
+      specimen <- gsub(pattern=file, replacement="", x=filename)
+      folder <- rep(subfolders[i], length(filename))
+      file_path <- paste0(folder, file)
+      temp_matrix <- cbind(specimen, filename, folder, file_path)
+      volume_data <- rbind(volume_data, temp_matrix)
+      rm(filename, specimen, folder, file_path, temp_matrix)
+    }
+    else {
+      rm(filename)
+    }
   }
-  
-  
   df_all <- as.data.frame(volume_data)
-
   return(df_all)
 }
 
